90dae8c219b357d69da6f776e3778597b5ea77dd
jason lee <i@jas0n1ee.me>
2014-09-22 21:10:24 +0800

    fix bug in detect,finish omg

--- Assignment4/linkgame/detect.m
+++ Assignment4/linkgame/detect.m
@@ -1,4 +1,4 @@
-function bool = detect(mtx, x1, y1, x2, y2)
+function bool = detect(mx, x1, y1, x2, y2)
     % ========================== 参数说明 ==========================
     
     % 输入参数中，mtx为图像块的矩阵，类似这样的格式：
@@ -14,9 +14,9 @@ function bool = detect(mtx, x1, y1, x2, y2)
     
     %% 在下面添加你的代码O(∩_∩)O
    	 
-    [m, n] = size(mtx);
+    [m, n] = size(mx);
 	mtx=[zeros(1,n+2);
-		zeros(m,1) mtx zeros(m,1);
+		zeros(m,1) mx zeros(m,1);
 		zeros(1,n+2)]
     [m, n] = size(mtx);
 	x1=x1+1;
@@ -25,6 +25,7 @@ function bool = detect(mtx, x1, y1, x2, y2)
 	y2=y2+1;
     bool = 0;
 	if mtx(x1,y1)==mtx(x2,y2)
+		%sprintf('checking%d %d %d %d',x1,y1,x2,y2)
 		if x1==x2 && sum(mtx(x1,min(y1,y2):max(y1,y2)))==(mtx(x1,y1)+mtx(x2,y2))
 			bool=1;
 			return;
@@ -34,14 +35,14 @@ function bool = detect(mtx, x1, y1, x2, y2)
 			return;
 		end
 		for i=1:m
-			path=sum(mtx(min(i,x1):max(i,x1),y1))+sum(mtx(i,min(y1,y2):max(y1,y2)))+sum(mtx(min(i,x2):max(i,x2),y2))
+			path=sum(mtx(min(i,x1):max(i,x1),y1))+sum(mtx(i,min(y1,y2):max(y1,y2)))+sum(mtx(min(i,x2):max(i,x2),y2));
 			if path==(mtx(x1,y1)+mtx(x2,y2)) || (path==(mtx(x1,y1)*3) && (i==x1||i==x2))
 				bool=1;
 				return;
 			end		
 		end
 		for j=1:n
-			path=sum(mtx(min(x1,x2):max(x1,x2),j))+sum(mtx(x1,min(j,y1):max(j,y1)))+sum(mtx(x2,min(j,y2):max(j,y2)))
+			path=sum(mtx(min(x1,x2):max(x1,x2),j))+sum(mtx(x1,min(j,y1):max(j,y1)))+sum(mtx(x2,min(j,y2):max(j,y2)));
 			if path==(mtx(x1,y1)+mtx(x2,y2)) || (path==(mtx(x1,y1)*3) &&(j==y1||j==y2))
 				bool=1;
 				return;
@@ -50,4 +51,4 @@ function bool = detect(mtx, x1, y1, x2, y2)
 	else
 		bool=0;
 	end
-
+end
